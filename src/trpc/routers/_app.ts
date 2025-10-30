3e5936d62fccf327329deb10ce0ec6a1af149b8b
Ravyar19 <your.email@example.com>
2025-10-30 12:41:33 +0300

    11: workflows crud

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,33 +1,8 @@
-import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "../init";
-import prisma from "@/lib/db";
-import { inngest } from "@/inngest/client";
-import { google } from "@ai-sdk/google";
-import { generateText } from "ai";
+import { createTRPCRouter } from "../init";
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testAI: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-    });
-    return { success: true, message: "job queued" };
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany({});
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "test@test.com",
-      },
-    });
-    return prisma.workflow.create({
-      data: {
-        name: "New Workflow",
-      },
-    });
-  }),
+  workflows: workflowsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
